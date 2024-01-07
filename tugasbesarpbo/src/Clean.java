86b8d532472796f5699500cf3f5ebf68be01e9b3
feri yanto rembang <145025356+feriyantorembang@users.noreply.github.com>
2024-01-07 22:35:14 +0700

    Add files via upload

---
+++ tugasbesarpbo/src/Clean.java
@@ -0,0 +1,13 @@
+
+
+//class clean
+public class Clean {
+    //constructor clean
+    public Clean(){
+    }
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
+    
+}
